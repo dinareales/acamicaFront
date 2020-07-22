c167f77907d52123ff781a96f9325f574a18aa5c
Dina Reales <dreales@workuniversity.co>
2020-07-21 20:13:07 -0500

    add images!

---
+++ src/Component/Product.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Product extends Component {
+    render() {
+        return (
+            <div>
+                Aca va la lista de productos!
+            </div>
+        );
+    }
+}
+
+export default Product;
---
+++ src/Component/ProductComp.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class ProductComp extends Component {
+    render() {
+        return (
+            <div>
+                aca armamos el componente
+            </div>
+        );
+    }
+}
+
+export default ProductComp;
